e9b9d8d5eb7e2deb0d862f819fa90397f537f595
chamchiii <misali9611@naver.com>
2024-10-15 22:01:14 +0900

    setupProxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,8 +1,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  // const apiServer = process.env.REACT_APP_API_SERVER;
-  const apiServer = process.env.REACT_APP_LOCAL_URL;
+  const apiServer = process.env.REACT_APP_API_SERVER;
+  // const apiServer = process.env.REACT_APP_LOCAL_URL;
 
   app.use(
     createProxyMiddleware(["/api", "/auth", "/image"], {
